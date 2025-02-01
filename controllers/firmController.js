6a369a188f0484f1125f6e303c9ec7410015173a
karthik5533 <karthiknaidu829839@gmail.com>
2025-01-31 21:13:29 -0500

    changes for frimid

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,67 +1,71 @@
-
-const Firm = require('../models/Firm')
-const Vendor = require('../models/Vendor') //Because we are adding the vendor to the firm
+const Firm = require("../models/Firm");
+const Vendor = require("../models/Vendor"); //Because we are adding the vendor to the firm
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, 'uploads/'); //Destination folder for where the uploaded images are stored
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); //For generating unique file name
-    }
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); //Destination folder for where the uploaded images are stored
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); //For generating unique file name
+  },
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-
-const addFirm = async(req,res)=>{
-    console.log("addFirm called"); //Debug log
-    try {
-    const {firmName, area, category, region, offer} = req.body;
+const addFirm = async (req, res) => {
+  console.log("addFirm called"); //Debug log
+  try {
+    const { firmName, area, category, region, offer } = req.body;
 
     const image = req.file ? req.file.filename : undefined;
 
-    console.log("Vendor ID from token:", req.vendorId);  // Debugging line
+    console.log("Vendor ID from token:", req.vendorId); // Debugging line
 
     const vendor = await Vendor.findById(req.vendorId);
-    if(!vendor){
-       return res.status(404).json({message: "Vendor not Found"})
+    if (!vendor) {
+      return res.status(404).json({ message: "Vendor not Found" });
+    }
+
+    if (vendor.firm.length > 0) {
+      return res.status(400).json({ message: "Vendor can have only one firm" });
     }
     // Create the firm, setting the vendor as an array
     const firm = new Firm({
-        firmName, 
-        area, 
-        category, 
-        region, 
-        offer,
-        image, 
-        vendor: [vendor._id]
+      firmName,
+      area,
+      category,
+      region,
+      offer,
+      image,
+      vendor: [vendor._id],
     });
 
     const savedFirm = await firm.save();
-    vendor.firm.push(savedFirm)
-    await vendor.save()
-    return res.status(200).json({message: "Firm Added Successfully"})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json("Internal Server Error")
+    const firmId = savedFirm._id;
+
+    vendor.firm.push(savedFirm);
+    await vendor.save();
+    return res.status(200).json({ message: "Firm Added Successfully", firmId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal Server Error");
+  }
+};
+
+const deleteFirmById = async (req, res) => {
+  try {
+    const firmId = req.params.firmId;
+    const deletedFirm = await Firm.findByIdAndDelete(firmId);
+
+    if (!deletedFirm) {
+      return res.status(404).json({ error: "No Firm Found" });
     }
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal Server Error");
+  }
+};
 
-const deleteFirmById = async(req,res)=>{
-    try {
-        const firmId = req.params.firmId;
-        const deletedFirm = await Firm.findByIdAndDelete(firmId);
-
-        if(!deletedFirm){
-            return res.status(404).json({error: "No Firm Found"})
-        }
-    } catch (error) {
-        console.error(error)
-        res.status(500).json("Internal Server Error")
-    } 
-}
-
-module.exports = {addFirm, upload, deleteFirmById};
+module.exports = { addFirm, upload, deleteFirmById };
--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,72 +1,77 @@
-const Vendor = require('../models/Vendor');
+const Vendor = require("../models/Vendor");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const dotEnv = require('dotenv');
+const dotEnv = require("dotenv");
 
-dotEnv.config()
-const secretKey = process.env.whatIsYourName
+dotEnv.config();
+const secretKey = process.env.whatIsYourName;
 
-const VendorRegister = async(req,res)=>{
-    const {username,email,password} = req.body;
-    try {
-        const vendorEmail = await Vendor.findOne({email});
-        if(vendorEmail){
-            return res.status(400).json("Email was already taken");
-        }
-        const hashedPassword = await bcrypt.hash(password,10);
-
-        const newVendor = new Vendor({
-            username,
-            email,
-            password : hashedPassword
-        });
-        await newVendor.save();
-        res.status(201).json({message:"Vendor Registered Successfully"});
-        console.log('Registered')
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error: "Internal Server Error"})
+const VendorRegister = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const vendorEmail = await Vendor.findOne({ email });
+    if (vendorEmail) {
+      return res.status(400).json("Email was already taken");
     }
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-const vendorLogin = async(req,res)=>{
-    const {email,password} = req.body
-    try {
-        const vendor = await Vendor.findOne({email});
-        if(!vendor || !(await bcrypt.compare(password,vendor.password))){
-           return res.status(401).json({error:"Invalid username or password"})
-        }
-        const token = jwt.sign({vendorId: vendor._id}, secretKey, {expiresIn:"1h"})
-        res.status(200).json({Success:"Login Successfull",token})
-        console.log(email,"This is token",token);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: "Internal Server error"});
+    const newVendor = new Vendor({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newVendor.save();
+    res.status(201).json({ message: "Vendor Registered Successfully" });
+    console.log("Registered");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const vendorLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const vendor = await Vendor.findOne({ email });
+    if (!vendor || !(await bcrypt.compare(password, vendor.password))) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
-}
+    const token = jwt.sign({ vendorId: vendor._id }, secretKey, {
+      expiresIn: "1h",
+    });
+    const vendorId = vendor._id;
+    res.status(200).json({ Success: "Login Successfull", token, vendorId });
+    console.log(email, "This is token", token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
 
-const getAllVendors = async(req,res)=>{
-    try {
-        const vendors = await Vendor.find().populate('firm');
-        res.json({vendors})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: "Internal Server error"});
+const getAllVendors = async (req, res) => {
+  try {
+    const vendors = await Vendor.find().populate("firm");
+    res.json({ vendors });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
+
+const getVendorById = async (req, res) => {
+  const vendorId = req.params.id;
+  try {
+    const vendor = await Vendor.findById(vendorId).populate("firm");
+    if (!vendor) {
+      return res.status(404).json({ error: "Vendor not found" });
     }
-}
+    const vendorFirmId = vendor.firm[0]._id;
+    res.status(200).json({ vendor, vendorId, vendorFirmId }); // vendor
+    console.log(vendor, vendorFirmId);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
 
-const getVendorById = async(req,res)=>{
-    const vendorId = req.params.id;
-    try {
-        const vendor = await Vendor.findById(vendorId).populate('firm');
-        if(!vendor){
-            return res.status(404).json({error: "Vendor not found"})
-        }
-        res.status(200).json({vendor})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: "Internal Server error"});
-    }
-}
-
-module.exports = {VendorRegister, vendorLogin, getAllVendors, getVendorById}
+module.exports = { VendorRegister, vendorLogin, getAllVendors, getVendorById };
